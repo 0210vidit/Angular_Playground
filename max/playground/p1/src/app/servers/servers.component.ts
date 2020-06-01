ec006f0d4e32dba2c96af690610bbeee4437574a
Vidit Shah <0210vidit@gmail.com>
2020-06-01 16:10:14 +0530

    phase1

--- max/playground/p1/src/app/servers/servers.component.ts
+++ max/playground/p1/src/app/servers/servers.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  disabeServerCreation = true;
+  serverCreationStatus = 'No Server was Created';
+  serverName = '';
+  serverCreated = false;
+  servers = ['Testsrver', 'Testserver2'];
+  constructor() {
+    setTimeout(() => {
+      this.disabeServerCreation = false;
+    }, 2000);
   }
 
+  ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
+  }
+  onUpdateServerName(value: string) {
+    this.serverName = value;
+  }
 }
